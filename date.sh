bf1865b5797f59f152ee0808710860a0cfd0dfc9
gitse <vargitse@gmail.com>
2016-09-19 12:39:21 +0300

    Update date.sh

--- date.sh
+++ date.sh
@@ -5,3 +5,15 @@
 d=$(<date.txt)
 
 date -s "$d"
+
+
+# mv date.sh /usr/bin/setDate
+# to update in 10 minutes, add setDate command to crontab: 
+# crontab -e 
+# 00 * * * * /usr/bin/setDate 
+# 10 * * * * /usr/bin/setDate 
+# 20 * * * * /usr/bin/setDate 
+# 30 * * * * /usr/bin/setDate 
+# 40 * * * * /usr/bin/setDate 
+# 50 * * * * /usr/bin/setDate 
+
